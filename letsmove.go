69c0325fcbc1f7f1a7be8b7565a26e79165c285b
Adam Bouqdib <adam@abemedia.co.uk>
2025-03-20 23:47:13 +0000

    feat: only support darwin, migrate deprecated constants (#1)

--- letsmove.go
+++ letsmove.go
@@ -1,17 +1,29 @@
+//go:build darwin
+// +build darwin
+
 // Package letsmove moves the running MacOS application to the Applications folder.
 package letsmove
 
-// Moves the running application to ~/Applications or /Applications if the former does not exist.
+/*
+#cgo CFLAGS: -I${SRCDIR}
+#cgo LDFLAGS: -L${SRCDIR} -framework Foundation -framework AppKit -framework Security
+
+void PFMoveToApplicationsFolderIfNecessary(void);
+int PFMoveIsInProgress(void);
+*/
+import "C"
+
+// MoveToApplications moves the running application to ~/Applications or /Applications if the former does not exist.
 // After the move, it relaunches app from the new location.
 // DOES NOT work for sandboxed applications.
 func MoveToApplications() {
-	moveToApplications()
+	C.PFMoveToApplicationsFolderIfNecessary()
 }
 
-// Check whether an app move is currently in progress.
+// IsMoveInProgress checks whether an app move is currently in progress.
 // Returns YES if LetsMove is currently in-progress trying to move the app to the Applications folder, or NO otherwise.
 // This can be used to work around a crash with apps that terminate after last window is closed.
 // See https://github.com/potionfactory/LetsMove/issues/64 for details.
 func IsMoveInProgress() bool {
-	return isMoveInProgress()
+	return C.PFMoveIsInProgress() == 1
 }
